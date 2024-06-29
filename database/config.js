cf446f09dc93176c8167f132c5905d1a74c18d87
gagan2125 <gagansk2125@gmail.com>
2024-06-29 14:52:58 +0530

    env commit

--- database/config.js
+++ database/config.js
@@ -5,10 +5,10 @@ dotenv.config();
 
 const connection = mysql.createPool({
   connectionLimit: 10,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "192.185.74.209",
+  user: "yourchoi_job_portal_db_user",
+  password: "M!P--c(s={DY",
+  database: "yourchoi_job_portal_db",
 });
 
 module.exports = connection;
